218769f19b36f3874448406893a5c8ee6fbcfcee
Brocolyyyu <126184612+Brocolyyyu@users.noreply.github.com>
2025-01-07 15:44:54 -0600

    Update easybroker.js

--- netlify/functions/easybroker.js
+++ netlify/functions/easybroker.js
@@ -1,7 +1,7 @@
 exports.handler = async function(event, context) {
     const response = await fetch("https://api.easybroker.com/v1/properties?limit=5", {
         headers: {
-            "X-Authorization": "TU_API_KEY",  // Reemplaza con tu API Key
+            "X-Authorization": "uvyru964fa0mwatr4auirtm005ssir",  // Reemplaza con tu API Key
             "Content-Type": "application/json"
         }
     });
